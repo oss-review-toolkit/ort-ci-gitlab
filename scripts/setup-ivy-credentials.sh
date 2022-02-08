1125af359e7d662d62edcc4fa704d78e39fcdfd9
Andriy Zhernovskyi <ext-andriy.zhernovskyi@here.com>
2022-02-08 16:47:59 +0200

    Add caching for SBT projects
    Signed-off-by: Andriy Zhernovskyi <ext-andriy.zhernovskyi@here.com>

--- scripts/setup-ivy-credentials.sh
+++ scripts/setup-ivy-credentials.sh
@@ -8,11 +8,12 @@ IVY_CREDS_URL=${IVY_CREDS_URL:-"example.com"}
 IVY_CREDS_USERNAME=${IVY_CREDS_USERNAME:-"john"}
 IVY_CREDS_PASSWORD=${IVY_CREDS_PASSWORD:-"example123"}
 
-cat > ~/.ivy2/.credentials <<EOF
+mkdir -p ${CI_PROJECT_DIR}/cache/ivy2
+cat > ${CI_PROJECT_DIR}/cache/ivy2/.credentials <<EOF
 realm=$IVY_CREDS_REALM
 host=$IVY_CREDS_URL
 user=$IVY_CREDS_USERNAME
 password=$IVY_CREDS_PASSWORD
 EOF
 
-chmod 0600 ~/.ivy2/.credentials
+chmod 0600 ${CI_PROJECT_DIR}/cache/ivy2/.credentials
